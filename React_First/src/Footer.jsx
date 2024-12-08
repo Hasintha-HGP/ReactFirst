e20c7635f32528f3668fb122bff56c711585bd73
Hasintha-HGP <phasintha34@gmail.com>
2024-12-08 13:29:04 +0530

    add

---
+++ React_First/src/Footer.jsx
@@ -0,0 +1,10 @@
+import App from "./App";
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy;{new Date().getFullYear()} All resiverd Pasindu Hasintha</p>
+        </footer>
+    );
+}
+export default Footer
