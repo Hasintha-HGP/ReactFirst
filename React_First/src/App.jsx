14ee9d4ead1e06491c806dfdd2c5fc5f42bd7023
Hasintha-HGP <phasintha34@gmail.com>
2024-12-08 13:43:48 +0530

    hi

--- React_First/src/App.jsx
+++ React_First/src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./Header.jsx"
 import Footer from "./footer.jsx";
 import Food from "./Food.jsx";
+import card from './card.jsx';
+
 function App() {
     return(
       <>
       <Header/>
       <Food/>
+      <card/>
       <Footer/>
       </>
     );
---
+++ React_First/src/card.jsx
@@ -0,0 +1,12 @@
+import hasi from "C:/Users/File/Desktop/hasi.jpg"
+
+function  card(){
+      return(
+        <div className="card">
+            <img src={hasi} alt="profile"></img>
+            <h2>Pasindu Hasintha</h2>
+            <p>I am Working On University of Kelaniya</p>
+        </div>
+      );
+}
+export default card
